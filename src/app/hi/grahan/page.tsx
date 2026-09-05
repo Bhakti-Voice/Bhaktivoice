8de7c6cdbe643921a618090fd40476fe422916e3
Arun Jain <jainarun889@gmail.com>
2026-09-05 22:38:53 +0530

    feat: add dedicated /hi/ routes for muhurat, vrat-upavas, and grahan pages

---
+++ src/app/hi/grahan/page.tsx
@@ -0,0 +1,6 @@
+import { withHindi } from "@/lib/i18n/hi-route";
+import EnDefault, { generateMetadata as enMeta } from "../../grahan/page";
+
+export const revalidate = 86400;
+export const generateMetadata = withHindi(enMeta);
+export default withHindi(EnDefault);
---
+++ src/app/hi/muhurat/page.tsx
@@ -0,0 +1,6 @@
+import { withHindi } from "@/lib/i18n/hi-route";
+import EnDefault, { generateMetadata as enMeta } from "../../muhurat/page";
+
+export const revalidate = 1800;
+export const generateMetadata = withHindi(enMeta);
+export default withHindi(EnDefault);
